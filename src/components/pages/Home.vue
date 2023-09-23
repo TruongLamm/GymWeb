59900e4c86b901d5be7c8ff4566b9e4fcbefb24b
truongvo <lamtruonq280902@gmail.com>
2023-09-23 17:32:38 +0700

    truong_523

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <v-cart width="100vw" min-height="100vh" height="wrap-content">
     <Banner></Banner>
-    <AboutUs></AboutUs>
     <Benefits></Benefits>
   </v-cart>
 </template>
 
 <script>
-import AboutUs from "../Atoms/AboutUs.vue";
 import Benefits from "../Organisms/Benefits.vue";
 import Banner from "../Organisms/Banner.vue";
 export default {
   components: {
     Banner,
-    AboutUs,
     Benefits,
   },
 };
