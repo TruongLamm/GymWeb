0e1d03ee72f7d8a9de3315075993cf2b7d580995
HienNLT <thaihiensp2002@gmail.com>
2023-09-23 15:57:07 +0700

    gi do

---
+++ src/components/Atoms/AboutUs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="btn-AboutUS" @click="handleButtonClick">About Us</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleButtonClick() {
+      alert("Button clicked!");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.btn-AboutUS {
+  background-color: #ff4601;
+  color: white;
+  border: 2px solid #ff4601;
+  border-radius: 50px;
+  padding: 10px 20px; /* Adjust padding as needed */
+  cursor: pointer;
+  font-size: 24px;
+  width: 237px;
+  height: 68px;
+  top: 522px;
+  left: 602px;
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  font-weight: 300;
+}
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <AboutUs></AboutUs>
+    <Benefits></Benefits>
+  </v-card>
+</template>
+
+<script>
+import AboutUs from "../Atoms/AboutUs.vue";
+import Benefits from "../Organisms/Benefits.vue";
+export default {
+  component: {
+    AboutUs,
+    Benefits,
+  },
+  components: { AboutUs, Benefits },
+};
+</script>
+<style></style>
