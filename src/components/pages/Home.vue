8b32aaf2ff8a832090f64bc2b843ae090030c31b
HienNLT <thaihiensp2002@gmail.com>
2023-10-06 11:38:41 +0700

    Instructor

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,16 +2,19 @@
   <v-cart width="100vw" min-height="100vh" height="wrap-content">
     <Banner></Banner>
     <Benefits></Benefits>
+    <Instructors></Instructors>
   </v-cart>
 </template>
 
 <script>
 import Benefits from "../Organisms/Benefits.vue";
 import Banner from "../Organisms/Banner.vue";
+import Instructors from "../Organisms/Instructors.vue";
 export default {
   components: {
     Banner,
     Benefits,
+    Instructors,
   },
 };
 </script>
